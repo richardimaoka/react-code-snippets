154617dd2784cd58b1d05ec0ad5c59a8f13b7321
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-09-08 01:06:01 +0900

    RippleButton1

---
+++ nextjs/app/components/buttons/RippleEffectButton1.tsx
@@ -0,0 +1,7 @@
+import styles from "./RippleEffectButton1.module.css";
+
+interface Props {}
+
+export function RippleEffectButton1(props: Props) {
+  return <button className={styles.component}>button</button>;
+}
--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "./components/buttons/Button";
+import { RippleEffectButton1 } from "./components/buttons/RippleEffectButton1";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
     <div className={styles.component}>
-      <Button />
+      <RippleEffectButton1 />
     </div>
   );
 }
