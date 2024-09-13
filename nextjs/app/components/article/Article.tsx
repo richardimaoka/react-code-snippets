1a5ca5b7338b479064486f045f32fb3352dfd69a
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-09-14 07:37:02 +0900

    article

--- nextjs/app/components/article/Article.tsx
+++ nextjs/app/components/article/Article.tsx
@@ -3,15 +3,5 @@ import styles from "./Article.module.css";
 interface Props {}
 
 export function Article(props: Props) {
-  return (
-    <div className={styles.component}>
-      <h1>This is an article about the US presidential election in 2024</h1>
-      <h2>A subtitle here</h2>
-      <p>Kamala Harris vs, Donald Trumphhh</p>
-      <h3>A sub sub title here</h3>
-      <p>Kamala Harris vs, Donald Trumphhh</p>
-      <p>Kamala Harris vs, Donald Trumphhh</p>
-      <p>Kamala Harris vs, Donald Trumphhh</p>
-    </div>
-  );
+  return <div className={styles.component}></div>;
 }
